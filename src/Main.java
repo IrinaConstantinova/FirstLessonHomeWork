51b3cc4d90d0820d7d3d4ae6e0f451f2af07a51a
Irina.Constantinova <irina.constantinova@gmail.com>
2024-03-05 19:23:08 +0300

    Added changes to README

--- src/Main.java
+++ src/Main.java
@@ -16,74 +16,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-//        Animal leva = new Lion("Leva", 200, LocalDate.now(), new Owner("Mike"), "yellow", true, 0.95f );
-//        Animal kuzea = new Lion("Kuzea", 250, LocalDate.now(), new Owner("Nick"), "white", false, 0.71f);
-//        Animal laika = new Dog("Laika", 13, LocalDate.now(), new Owner("Alex"), "black", 0.33f);
-//        Animal frank = new Crocodile("Frank", 150, LocalDate.now(), new Owner("Lisa"), "green", 1.64f );
-//        Animal goldy = new Fish("Goldy", 0.5f, LocalDate.now(), new Owner("Bob"), "gold", 0.15f);
-//        Animal george = new Penguin("George", 5, LocalDate.now(), new Owner("Anna"), "black",0.51f);
-//        Animal funny = new Duck("Funny", 1.5f, LocalDate.now(), new Owner("Elza"),"white", 0.36f);
-//        Animal puffy = new Rabbit("Puffy", 2, LocalDate.now(), new Owner("Oleg"),"brown", 0.23f);
-//        Animal kitty = new Cat("Kitty", 4, LocalDate.now(), new Owner("Elena"), "gray", 0.32f);
-//        Animal sam = new Parrot("Sam", 0.50f, LocalDate.now(), new Owner("Lana"), "blue");
-
-//        Personal fiona = new Nurse("Fiona", 1, "Chief nurse");
-//        Personal anna = new Nurse("Anna", 3, "Assistant surgeon");
-//        Personal alan = new Doctor("Alan", 2, "Chief doctor");
-//        Personal dana = new Doctor("Dana", 4, "Doctor");
-//        Doctor alf = new Doctor("Bil", 6, "Doctor");
-//        Nurse nora= new Nurse("Nora", 8, "Junior nurse");
-//
-//        VeterinaryClinic clinic = new VeterinaryClinic();
-//
-//        clinic.addPersonal(alf);
-//        clinic.addPersonal(nora);
-//        clinic.addPersonal(fiona);
-//        clinic.addPersonal(anna);
-//        clinic.addPersonal(alan);
-//        clinic.addPersonal(dana);
-//
-//        System.out.println(clinic.getPersonals());
-//        System.out.println("---------------------");
-//        System.out.println("\n" + "All veterinary clinic doctors: " );
-//        System.out.println(clinic.getDoctors());
-//        System.out.println("\n" + "All veterinary clinic nurses: " );
-//        System.out.println(clinic.getNurses());
-
-
-//        clinic.removePersonal(fiona);
-//        System.out.println(clinic.getPersonals());
-//        System.out.println("--------------------");
-//
-//        clinic.addAnimalToClinic(sam);
-//        clinic.addAnimalToClinic(kitty);
-//        clinic.addAnimalToClinic(puffy);
-//        clinic.addAnimalToClinic(funny);
-//        clinic.addAnimalToClinic(george);
-//        clinic.addAnimalToClinic(goldy);
-//        clinic.addAnimalToClinic(frank);
-//        clinic.addAnimalToClinic(laika);
-//        clinic.addAnimalToClinic(kuzea);
-//        clinic.addAnimalToClinic(leva);
-//        System.out.println(clinic.getAnimals());
-//        System.out.println("--------------------");
-
-//        clinic.dischargeFromClinic(leva);
-//        System.out.println(clinic.getAnimals());
-
-//        System.out.println("--------------------");
-//        System.out.println(kitty);
-//        alf.heal(kitty);
-//        alf.prescribeDiagnose(kitty);
-//        nora.vaccinate(kitty);
-
-//        System.out.println("\n" + "Animals that can fly: " );
-//        System.out.println(clinic.getFlyable());
-//        System.out.println("\n" + "Animals that can run: ");
-//        System.out.println(clinic.getRunable());
-//        System.out.println("\n" + "Animals that can swim: ");
-//        System.out.println(clinic.getSwimmable());
-
 
         MedComponent asitromin = new Asitromin("Asitromin", 30, 10);
         MedComponent eritromicin = new Eritromicin("Eritromicin", 10, 100);
@@ -125,30 +57,6 @@ public class Main {
 //        Collections.sort(medicineList);
 //        System.out.println(medicineList);
 
-
-
-//        Medicine2 med1 = new Medicine2();
-//        med1.addComponent(asitromin).addComponent(eritromicin).addComponent(penicilin);
-////        while (med1.hasNext()){
-////            System.out.println(med1.next());
-////        }
-//        for (MedComponent component : med1){
-//            System.out.println(component);
-//        }
-//        List<MedComponent> medComponentList = new ArrayList<>();
-//        medComponentList.add(asitromin);
-//        medComponentList.add(eritromicin);
-//        medComponentList.add(penicilin);
-//        System.out.println(medComponentList);
-
-//        Collections.sort(medComponentList);
-//        System.out.println(medComponentList);
-
-//        Collections.sort(medComponentList, Comparator.reverseOrder());
-//        System.out.println(medComponentList);
-
-//        Collections.sort(medComponentList, (comp1, comp2) -> (int)(comp1.getWeight() - comp2.getWeight()));
-//        System.out.println(medComponentList);
 
     }
 }
